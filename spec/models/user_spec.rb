a2d0c0f5d050cbb3b5fb29841680670930cc7fdb
Adrianna Gilmore <a_inman33@yahoo.com>
2021-01-28 11:10:03 -0800

    user was testing too much, removed all but instantiation and email tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,49 +10,4 @@ RSpec.describe User, type: :model do
     expect(user).to respond_to(:email)
   end
 
-  it 'has an encrypted_password' do
-    user = User.new
-    expect(user).to respond_to(:encrypted_password)
-  end
-
-  it 'has a reset_password_token' do
-    user = User.new
-    expect(user).to respond_to(:reset_password_token)
-  end
-
-  it 'has a reset_password_sent_at' do
-    user = User.new
-    expect(user).to respond_to(:reset_password_sent_at)
-  end
-
-  it 'has a remember_created_at' do
-    user = User.new
-    expect(user).to respond_to(:remember_created_at)
-  end
-
-  it 'has a confirmation_token' do
-    user = User.new
-    expect(user).to respond_to(:confirmation_token)
-  end
-
-  it 'has a confirmation_sent_at' do
-    user = User.new
-    expect(user).to respond_to(:confirmation_sent_at)
-  end
-
-  it 'has a confirmed_at' do
-    user = User.new
-    expect(user).to respond_to(:confirmed_at)
-  end
-
-  it 'has an unconfirmed_email' do
-    user = User.new
-    expect(user).to respond_to(:unconfirmed_email)
-  end
-
-  it 'has a role' do
-    user = User.new
-    expect(user).to respond_to(:role)
-  end
-
 end
