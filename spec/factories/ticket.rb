06e61323bd7d835361af04e7b41f20df99eb3fde
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-04 11:16:27 -0800

    things are a little broken, need to add my region factory

--- spec/factories/ticket.rb
+++ spec/factories/ticket.rb
@@ -2,10 +2,14 @@ FactoryBot.define do
   factory :ticket do
     name { 'Fake ticket' }
     description { 'Fake description' }
-    phone { '541-591-2880' }
+    phone { '+1541-591-2880' }
     closed { false }
     closed_at { nil }
     region
     resource_category
+
+    trait :closed do
+      closed { true }
+    end
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -26,4 +26,33 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to respond_to(:closed_at)
   end
 
+  describe 'scopes' do
+
+    describe 'closed' do
+      it 'contains closed tickets' do
+        closed_ticket = create(:ticket, :closed)
+        expect(Ticket.closed).to include(closed_ticket)
+      end
+      it 'contains non-closed tickets' do
+        non_closed_ticket = create(:ticket)
+        expect(Ticket.closed).to_not include(non_closed_ticket)
+      end
+    end
+
+    describe 'region' do
+      it 'returns a ticket for a region given that region\'s id' do
+        ticket = create(:ticket)
+        region = ticket.region
+        expect(Ticket.region(region.ed)).to include(ticket)
+      end
+      it 'does not return a ticket for a different region id' do
+        ticket = create(:ticket)
+        region = ticket.region
+        other_region_ticket = build(:ticket)
+        other_region_ticket.region = create(:region, name: 'New Fake Region')
+        expect(Ticket.region(region.id)).to_not include(other_region_ticket)
+      end
+    end
+  end
+
 end
