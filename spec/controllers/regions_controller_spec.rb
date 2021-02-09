984f35fbe1bf682fb79ad90116d5ea9a6c6862e1
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-09 12:07:55 -0800

    not working, need to build organization factory I think...

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -3,11 +3,41 @@ require 'rails_helper'
 RSpec.describe RegionsController, type: :controller do
 
   context 'unauthenticated users' do
-    describe '#index' do
-      it 'redirects to the sign_in screen' do
-        get :index
-        expect(response).to redirect_to(new_user_session_url)
-      end
+
+    
+    it 'redirects to the sign_in screen' do
+      get :index
+      expect(response).to redirect_to(new_user_session_url)
+      get :show, params: {id: 'fake'} 
+      expect(response).to redirect_to(new_user_session_url)
+      get :new
+      expect(response).to redirect_to(new_user_session_url)
+      post :create
+      expect(response).to redirect_to(new_user_session_url)
+      get :edit, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+    end
+
+    #edit needs params
+    #update needs params
+    #destroy needs params
+    #rails routes <-use this to look up the urls
+
+  end
+
+  context 'organization users' do
+    before do
+        allow(controller).to receive(:current_user).and_return(build(:organization))
+    end
+    it 'redirects to the dashboard' do
+      get :index
+      expect(response).to redirect_to(dashboard_url)
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      get :new
+      expect(response).to redirect_to(dashboard_url)
+      post :create
+      expect(response).to redirect_to(dashboard_url)
     end
   end
 
