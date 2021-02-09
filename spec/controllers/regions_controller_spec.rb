f7cd98f36f946b512c7d3174f553fac36cfc3af0
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-09 11:28:31 -0800

    first controller test

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  context 'unauthenticated users' do
+    describe '#index' do
+      it 'redirects to the sign_in screen' do
+        get :index
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+  end
+
 end
