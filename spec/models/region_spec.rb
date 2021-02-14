27194c8cb8fa4070840eb4d4eca7fb429e3aada4
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-14 00:05:01 -0800

    added validators to region_spec

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -21,4 +21,14 @@ RSpec.describe Region, type: :model do
     expect(region.to_s).to eq(name)
   end
 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name) 
+          .is_at_least(1)
+          .is_at_most(255)
+          .on(:create) }
+    it { is_expected.to validate_uniqueness_of(:name)
+          .case_insensitive }
+  end
+
 end
