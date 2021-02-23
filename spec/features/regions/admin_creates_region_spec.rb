53929cdc100cd306af44a4f0cd08a4f0d265dbee
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-23 11:27:15 -0800

    soemthing went wrong..need to fix

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+  let(:admin) { create(:user, :admin) }
 
+  it 'succeeds' do
+    region_name = 'Fake Region Meow'
+    log_in_as(admin)
+    click_on 'Regions'
+    click_on 'Add Region'
+    fill_in 'Name', with: region_name
+    click_on 'Add Region'
+    expect(page.body).to have_text('Region successfully created.')
+    expect(page.body).to have_text(region_name)
+  end
+      
 end
