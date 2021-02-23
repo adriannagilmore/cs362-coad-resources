9476549b740a22aeb9a80f9ead32d2cc8833dc98
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-23 10:27:37 -0800

    not sure what changes did not get commited

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+  context 'unauthenticated users' do
+    
+    it 'redirects to the sign_in screen' do
+      get :new
+      expect(response).to redirect_to(new_user_session_url)
+      post :create
+      expect(response).to redirect_to(new_user_session_url)
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+      get :capture, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+      get :release, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+      get :close, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+      get :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+    end
+    
+  end
+
 end
