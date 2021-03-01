bfb46f53bcd8a4e6794f46786d4f6040adae519a
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-28 23:51:54 -0800

    added 'caputre a ticket' feature test

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-
+  let(:organization) { create(:user, :organization) }
+  let(:ticket) { create(:ticket) }
+      
+  it 'succeeds' do
+    log_in_as(organization) 
+    fake_ticket = ticket
+    click_on 'Dashboard'
+    click_on 'Tickets'
+    click_on ticket.name
+    click_on 'Capture'
+    click_on ticket.name
+    expect(page.body).to have_text('Captured')
+  end
 end
