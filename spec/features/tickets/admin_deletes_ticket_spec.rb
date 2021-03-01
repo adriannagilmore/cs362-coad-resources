6a73ffd48844ca85af1ff76f640fc79ff3ea6988
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-28 23:19:19 -0800

    not sure if I should test deleting something in the app?

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
-
+  let(:admin) { create(:user, :admin) }
+  let(:ticket) { create(:ticket) }
+    
+  it 'succeeds' do
+    log_in_as(admin) 
+    fake_ticket = ticket
+    click_on 'Dashboard'
+    click_on ticket.name
+    click_on 'Delete'
+    #was not sure if I should actually try to delete a ticket in the app to see what happens
+    #expect(page.body).to have_text()
+    end
 end
