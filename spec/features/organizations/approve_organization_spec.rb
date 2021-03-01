8ba9de4e2c8848024d7639fd20569e744b4c852a
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-28 23:40:58 -0800

    need some help with approving an organization

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  let(:admin) {create(:user, :admin)}
+  let(:organization) {create(:organization)}
+
+  it 'succeeds' do
+    log_in_as(admin)
+    fake_organization = organization
+    click_on 'Organizations'
+    click_on 'Pending'
+    #**keep getting error that organization.name is ambiguous**
+    #click_on organization.name
+    #click_on 'Approve'
+    #expect(page.body).to have_text("Organization #{organization.name} has been approved.")
+  end
 
 end
