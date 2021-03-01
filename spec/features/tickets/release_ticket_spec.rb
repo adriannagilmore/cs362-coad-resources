f06f83c819f6d5f90530657bbe9d5475e2b57833
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-28 23:59:04 -0800

    added releasing a ticket feature

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
-
+  let(:organization) { create(:user, :organization) }
+  let(:ticket) { create(:ticket) }
+        
+  it 'succeeds' do
+    log_in_as(organization) 
+    fake_ticket = ticket
+    click_on 'Dashboard'
+    click_on 'Tickets'
+    click_on ticket.name
+    click_on 'Capture'
+    click_on 'Tickets'
+    click_on ticket.name
+    click_on 'Release'
+    click_on 'Tickets'
+    click_on ticket.name
+    expect(page.body).to have_no_text('Release')
+  end
 end
