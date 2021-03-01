6229a215f4e101315f956267856d5ed08bede596
Adrianna Gilmore <a_inman33@yahoo.com>
2021-02-28 23:16:14 -0800

    working on feature tests

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  let(:admin) { create(:user, :admin) }
+  let(:region) { create(:region) }
 
+  it 'succeeds' do
+    fake_region = region
+    log_in_as(admin)
+    click_on 'Regions'
+    click_on region.name
+    click_on 'Delete'
+   # expect(page.body).to have_text()
+    end
 end
--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
-
+  let(:admin) { create(:user, :admin) }
+  let(:ticket) { create(:ticket) }
+  
+  it 'succeeds' do
+    log_in_as(admin) 
+    fake_ticket = ticket
+    click_on 'Dashboard'
+    click_on ticket.name
+    click_on 'Close'
+    click_on ticket.name
+    expect(page.body).to have_text('Closed')
+  end
 end
