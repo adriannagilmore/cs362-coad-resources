8d7ea405a21eab11bc208d77dd23a001c433d3e6
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-02 11:50:52 -0800

    added organization users to resource categories controller

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -23,4 +23,30 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
   end
 
+  context 'organization users' do
+    
+    before do
+      organization_user = build(:user, :organization)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user)
+    end
+
+    it 'redirects to the dashboard' do
+      get :index
+      expect(response).to redirect_to(dashboard_url)
+      get :new
+      expect(response).to redirect_to(dashboard_url)
+      post :create
+      expect(response).to redirect_to(dashboard_url)
+      get :update, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      get :activate, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      get :deactivate, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      get :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
 end
