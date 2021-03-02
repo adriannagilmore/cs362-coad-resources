945dc9b73bdc6c48eefa0f565e335fb8003183b1
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-02 11:36:08 -0800

    added ticket controller

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,25 +2,18 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-  # context 'unauthenticated users' do
+  context 'organization users' do
+    before do
+      organization_user = build(:user, :organization)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user)
+    end
     
-  #   it 'redirects to the sign_in screen' do
-  #     get :new
-  #     expect(response).to redirect_to(new_user_session_url)
-  #     post :create
-  #     expect(response).to redirect_to(new_user_session_url)
-  #     get :show, params: {id: 'fake'}
-  #     expect(response).to redirect_to(new_user_session_url)
-  #     get :capture, params: {id: 'fake'}
-  #     expect(response).to redirect_to(new_user_session_url)
-  #     get :release, params: {id: 'fake'}
-  #     expect(response).to redirect_to(new_user_session_url)
-  #     get :close, params: {id: 'fake'}
-  #     expect(response).to redirect_to(new_user_session_url)
-  #     get :destroy, params: {id: 'fake'}
-  #     expect(response).to redirect_to(new_user_session_url)
-  #   end
+    it 'redirects to the dashboard' do
+      get :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+    end
     
-  # end
+  end
 
 end
