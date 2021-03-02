96920ab15f6f20b3bbc549d6be2b01b23efbe806
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-02 11:42:40 -0800

    added dashboard controller

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+  it 'redirects to the sign_in screen' do
+    get :index
+    expect(response).to redirect_to(new_user_session_url)
+  end
+  
 end
