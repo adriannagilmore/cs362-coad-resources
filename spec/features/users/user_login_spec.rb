6c80dd2192803dd858c9175a98daf9a35151efd7
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-09 23:19:02 -0800

    user login feature working

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  let(:organization) { create(:user, :organization) }
 
+  it 'succeeds' do
+    user = organization
+    visit dashboard_path
+    click_on 'Log in'
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: 'password'
+    click_on 'Sign in'
+    expect(page.body).to have_text('Signed in successfully')
+  end
 end
