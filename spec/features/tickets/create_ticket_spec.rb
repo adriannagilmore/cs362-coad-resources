6dddbe870b2bd74f2fb24d34040e0f17954a3a50
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-09 23:01:38 -0800

    create ticket feature working

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
+  let(:region) { create(:region) }
+  let(:resource_category) { create(:resource_category) }
 
+  it 'succeeds' do
+    fake_region = region
+    fake_category = resource_category
+    visit dashboard_path
+    click_on 'Disaster Resource Network'
+    click_on 'Get Help'
+    fill_in 'Name', with: 'Fake Name'
+    fill_in 'Phone Number', with: '541-222-3333'
+    select(region, from: 'Region')
+    select(resource_category, from: 'Resource Category')
+    fill_in 'Description', with: 'Help me'
+    click_on 'Send this help request'
+    expect(page.body).to have_text('Ticket Submitted')
+  end
 end
