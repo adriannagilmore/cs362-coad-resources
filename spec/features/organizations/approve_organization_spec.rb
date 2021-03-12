c0d4580696ad273059667e5c831ff4d3a92d6432
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-11 23:24:55 -0800

    having issues with approve_organization_spec

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -9,8 +9,7 @@ RSpec.describe 'Approving an organization', type: :feature do
     fake_organization = organization
     click_on 'Organizations'
     click_on 'Pending'
-    #**keep getting error that organization.name is ambiguous**
-    #click_on organization.name
+    click_on fake_organization.name, match: :first
     #click_on 'Approve'
     #expect(page.body).to have_text("Organization #{organization.name} has been approved.")
   end
