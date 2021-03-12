4b6b5fa68ab49f91f6e718711a53482bab3c21ac
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-11 23:28:53 -0800

    started adding to create_organization_application_spec but I can't seem to figure out how to do that in the app. Will try again later

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
+  let(:admin) {create(:user, :admin)}
+  let(:organization) {create(:organization)}
 
+  it 'succeeds' do
+    log_in_as(admin)
+    fake_organization = organization
+  end
 end
