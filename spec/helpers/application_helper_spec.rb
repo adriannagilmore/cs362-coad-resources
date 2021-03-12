fffd8380ba4ba7f45180f5c5dd6be32b4b7f014e
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-11 23:00:27 -0800

    added to application_helper_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-
+  describe '#full_title' do
+    it 'returns base_title' do
+      page_title = double()
+      allow(page_title).to receive(:empty?) {true} 
+      expect(full_title(page_title)).to eq('Disaster Resource Network')
+    end
+    it 'returns page_title | base_title' do
+      page_title = double()
+      allow(page_title).to receive(:empty?) {false}
+      expect(full_title(page_title)).to eq("#{page_title} | Disaster Resource Network")
+    end
+  end
 end
