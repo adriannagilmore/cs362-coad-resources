02f93cce2646c5fcdad0a3641f9527848bca5878
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-11 22:40:04 -0800

    added dashboard_helper_spec

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -11,5 +11,39 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe DashboardHelper, type: :helper do
-
+  describe '#dashboard_for' do
+    context 'admin_users' do
+      it 'returns admin_dashboard' do
+        user = double()
+        allow(user).to receive(:admin?) {true}
+        expect(dashboard_for(user)).to eq('admin_dashboard')
+      end
+    end
+    context 'organiation users with a submitted organization' do
+      it 'returns organization_submitted_dashboard' do
+        user = double()
+        allow(user).to receive(:admin?) {false}
+        allow(user).to receive_message_chain(:organization, :submitted?) {true}
+        expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+      end
+    end
+    context 'organiation users with an approved organization' do
+      it 'returns organization_approved_dashboard' do
+        user = double()
+        allow(user).to receive(:admin?) {false}
+        allow(user).to receive_message_chain(:organization, :submitted?) {false}
+        allow(user).to receive_message_chain(:organization, :approved?) {true}
+        expect(dashboard_for(user)).to eq('organization_approved_dashboard')
+      end
+    end
+    context 'organiation users without an organization' do
+      it 'returns organization_application_dashboard' do
+        user = double()
+        allow(user).to receive(:admin?) {false}
+        allow(user).to receive_message_chain(:organization, :submitted?) {false}
+        allow(user).to receive_message_chain(:organization, :approved?) {false}
+        expect(dashboard_for(user)).to eq('create_application_dashboard')
+      end
+    end
+   end
 end
