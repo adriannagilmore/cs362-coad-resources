5f9abd6348e0df1597d142e4bba7e28f1659febc
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-11 23:42:16 -0800

    having some issues with the application, while trying to update update_organization_spec

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
-
+  let(:user) {create(:user, :organization)}
+  let(:organization) {create(:organization)}
+  
+  it 'succeeds' do
+    log_in_as(user)
+    fake_organization = organization
+    click_on 'Dashboard'
+    click_on 'Edit Organization'
+    fill_in 'Name', with: "Fake Name"
+    click_on "Update Resource"
+    #nothing happened when I clicked the Update Resource button
+  end
 end
